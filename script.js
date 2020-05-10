7a607709d5e1a66eca862117ad8430a2aef290a1
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-05-10 21:20:24 +0500

    more progress made on logic

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const game = (function() {
 
+    const _playerOne = player('Axe', 'X');
+    const _playerTwo = player('Leshrac', 'O');    
+    let _current = _playerOne;
+
     const _winningCombos = [ // all possible winning combinations in a game of TTT
-        [1, 2, 3],
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
         [1, 4, 7],
-        [1, 5, 9],
+        [2, 4, 6],
         [2, 5, 8],
-        [3, 5, 7],
-        [3, 6, 9],
-        [4, 5, 6],
-        [7, 8, 9]
+        [3, 4, 5],
+        [6, 7, 8]
     ];
     
+    // updated every turn
     const _boardState = [null, null, null, null, null, null, null, null, null];
+
+    const _alternatePlayer = () => {
+        if (_current.symbol === 'X') {
+            _current = _playerTwo;
+        } else {
+            _current = _playerOne;
+        }
+    }
+
+    const _checkResult = () => {
+        _winningCombos.forEach(combo => {
+            if (_boardState[combo[0]] === _boardState[combo[1]] && 
+                _boardState[combo[1]] === _boardState[combo[2]] &&
+                _boardState[combo[0]] !== null) {
+                alert(`${_boardState[combo[0]]} WINS`);
+                board.forEach(cell => cell.addEventListener('click', () => {}));
+            }
+        });
+    }
 
     const board = [
         document.getElementById('cell-1'),
@@ -25,15 +49,22 @@ const game = (function() {
         document.getElementById('cell-9')
     ];
 
-    function updateBoard(sign) {
+    // symbol placeholder setup
+
+    function turn() {
         if (this.textContent.length !== 0) {
             alert('Full');
         } else {
-            this.textContent = sign;
+            this.textContent = _current.symbol;
         }
+        _current.cells.push(Number(this.id[5]));
+        _boardState[Number(this.id[5]) - 1] = _current.symbol;
+        _checkResult();
+        console.log(_boardState);
+        _alternatePlayer(); // switch player
     }
 
-    board.forEach(cell => cell.addEventListener('click', updateBoard.bind(cell, 'O')));
+    board.forEach(cell => cell.addEventListener('click', turn.bind(cell)));
 
 
     return {
@@ -42,3 +73,14 @@ const game = (function() {
 
 /* END OF MODULE */
 })();
+
+
+// Player Factory
+function player (name, symbol) {
+    const cells = [];
+    return {
+        name,
+        symbol,
+        cells
+    };
+}
