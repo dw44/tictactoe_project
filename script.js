b53bac8b3e7c6eda869ff04e55f7b96ada5e154e
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-05-14 19:33:22 +0500

    redid logic and presentation

--- script.js
+++ script.js
@@ -1,144 +1,224 @@
+"use strict";
+
+/* ===================================================================================== */
+                                /* GAME BOARD OBJECT */
+/* ===================================================================================== */
 const gameBoard = (function() {
       
-    const board = [
-        document.getElementById('cell-0'),
-        document.getElementById('cell-1'),
-        document.getElementById('cell-2'),
-        document.getElementById('cell-3'),
-        document.getElementById('cell-4'),
-        document.getElementById('cell-5'),
-        document.getElementById('cell-6'),
-        document.getElementById('cell-7'),
-        document.getElementById('cell-8')        
-    ];
+  const board = [
+      document.getElementById('cell-0'),
+      document.getElementById('cell-1'),
+      document.getElementById('cell-2'),
+      document.getElementById('cell-3'),
+      document.getElementById('cell-4'),
+      document.getElementById('cell-5'),
+      document.getElementById('cell-6'),
+      document.getElementById('cell-7'),
+      document.getElementById('cell-8')        
+  ];
 
-    const updateBoard = (marker, location) => {
-        board[location].textContent = marker;
-    }
+  const updateBoard = (marker, location) => {
+      board[location].textContent = marker;
+  }
 
-    return {
-        board,
-        updateBoard,
-    };
+  const clearBoard = () => {
+    board.forEach(cell => {
+      cell.textContent = '';
+    });
+  }
+
+  return {
+      board,
+      updateBoard,
+      clearBoard
+  };
 
 })();
 
 /* ===================================================================================== */
-/* GAME LOGIC */
+                                /* PLAYER OBJECT */
+/* ===================================================================================== */
+
+const playerFactory = function (name, marker, type='human') {
+  let _score = 0;
+  const increaseScore = () => ++_score;
+  const resetScore = () => _score = 0;
+  const getScore = () => _score;
+
+  return {
+      name, 
+      marker,
+      type,
+      increaseScore,
+      resetScore,
+      getScore
+  };
+}
+
+/* ===================================================================================== */
+                                    /* GAME LOGIC */
+/* ===================================================================================== */
 
 const game = (function() {
 
-    const winningCombos = [ // all possible winning combinations in a game of TTT
-        [0, 1, 2],
-        [0, 3, 6],
-        [0, 4, 8],
-        [1, 4, 7],
-        [2, 4, 6],
-        [2, 5, 8],
-        [3, 4, 5],
-        [6, 7, 8]
-    ];
+  const initBtn = document.getElementById('submit-players');
+  const playerTypes = document.getElementById('player-type');
+  const modal = document.getElementById('modal');
+  const initForm = document.getElementById('new-game');
+  const winnerDisplay = document.getElementById('show-winner');
+  const roundWinner = document.getElementById('winner-name');
+  const newRoundBtn = document.getElementById('soft-reset');
+  const hardResetBtn = document.getElementById('reset');
+  const playerOneTitle = document.getElementById('player-1-title');
+  const playerTwoTitle = document.getElementById('player-2-title');
+  const playerOneScore = document.getElementById('player-1-score');
+  const playerTwoScore = document.getElementById('player-2-score');
 
-    const boardState = [null, null, null, null, null, null, null, null, null];
-    const softResetButton = document.getElementById('soft-reset');
-    const hardResetButton = document.getElementById('reset');
-    const playerOne = playerFactory('Player 1', 'X');
-    const playerTwo = playerFactory('Player 2', 'O');
+  const boardState = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const winningCombos = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]; // All possible winning combinations
 
-    let win = false;
-    let activePlayer = playerOne;
+  let playerOne;
+  let playerTwo;    
+  let activePlayer;
+  
+  initBtn.addEventListener('click', initializePlayers);
+  newRoundBtn.addEventListener('click', newRound);
+  hardResetBtn.addEventListener('click', hardReset);
+  gameBoard.board.forEach(cell => cell.addEventListener('click', turn));
 
-    gameBoard.board.forEach(cell => cell.addEventListener('click', turn));
-    softResetButton.addEventListener('click', resetGame);
-    hardResetButton.addEventListener('click', hardReset);
-
-    function cleanBoard() {
-        for (let i = 0; i < 9; ++i) {
-            boardState[i] = null;
-            gameBoard.board[i].textContent = '';
-            activePlayer = playerOne;
-        } 
+  const createPlayers = function(ai = false) { // Tested - Working
+    if (ai === true) {
+      playerOne = playerFactory(document.getElementById('p1-name').value || 'Axe', 'X');
+      playerTwo = playerFactory(document.getElementById('p2-name').value || 'Computer', 'O', 'computer');
+    } else {
+      playerOne = playerFactory(document.getElementById('p1-name').value || 'Axe', 'X');
+      playerTwo = playerFactory(document.getElementById('p2-name').value || 'Leshrac', 'O');
     }
-        
-    function resetGame() {
-        win = false;
-        cleanBoard();
-        enableGrid();
-        toggleDisabled(softResetButton, hardResetButton);
-    }
+  }
 
-    function togglePlayer() { // switch players
-        activePlayer = (activePlayer.marker === playerTwo.marker) ? playerOne : playerTwo;
+  const clearState = () => {
+    for (let i = 0; i < 9; ++i) {
+      boardState[i] = i;
     }
+  }
 
-    function toggleDisabled(...args) {
-        args.forEach(arg => arg.disabled = !arg.disabled);
+  function initializePlayers() { // Tested - Working
+    if (playerTypes.value === 'computer') createPlayers(true);
+    else createPlayers();
+    // Initializing early game variables
+    activePlayer = playerOne;
+    playerOneTitle.textContent = playerOne.name;
+    playerTwoTitle.textContent = playerTwo.name;    
+    initForm.style.display = 'none';
+    modal.style.display = 'none';
+  }
+
+  function newRound() { // Tested - Working
+    clearState();
+    gameBoard.clearBoard();
+    activePlayer = playerOne;
+    modal.style.display = 'none';
+    winnerDisplay.style.display = 'none';
+  }
+
+  function hardReset() {
+    clearState();
+    gameBoard.clearBoard();
+    playerOne = undefined;
+    playerOneTitle.textContent = 'Axe';
+    playerOneScore.textContent = '0';
+    playerTwo = undefined;
+    playerTwoTitle.textContent = 'Leshrac';
+    playerTwoScore.textContent = '0';    
+    winnerDisplay.style.display = 'none';
+    initForm.style.display = 'flex';
+  }
+
+  const togglePlayer = function() { // Tested - Working
+    activePlayer = (activePlayer.marker === playerOne.marker) ? playerTwo : playerOne;
+  }
+
+  const win = function(board, marker) { // Tested - Working
+    for (const combo of winningCombos) {
+      if (combo.every(index => board[index] === marker)) return true;
     }
+    return false;
+  }
 
-    function endGame() {
-        winningCombos.forEach(combo => {
-            if (boardState[combo[0]] === boardState[combo[1]] && 
-                boardState[combo[1]] === boardState[combo[2]] &&
-                boardState[combo[0]] !== null) {
-                    const winner = (activePlayer.marker === playerOne.marker) ? playerTwo : playerOne;
-                    win = true;
-                    winner.increaseScore();
-                    document.getElementById('player-1-score').textContent = playerOne.getScore();
-                    document.getElementById('player-2-score').textContent = playerTwo.getScore();
-                    alert(`${winner.name} wins!!!`);
-                    disableGrid();
-                    toggleDisabled(softResetButton, hardResetButton, ...gameBoard.board);
-                }
-        });
-        if (boardState.every(marker => marker !== null) && !win) {
-            alert('Match Drawn!!!');
-            disableGrid();
-            toggleDisabled(softResetButton, hardResetButton);
-        }
+  const draw = function(board) { // Tested - Working
+    return board.filter(n => n !== 'X' && n !== 'O').length === 0 &&
+          !win(board, 'X') &&
+          !win(board, 'O');
+  }
+
+  const gameOver = function() { // Tested - Working
+    return win(boardState, 'X') || win(boardState, 'O') || draw(boardState);
+  }
+
+  const endGameHandler = function() {
+    if (win(boardState, 'X')) {
+      playerOne.increaseScore();
+      playerOneScore.textContent = playerOne.getScore();
+      var result = `${playerOne.name} Wins`;
     }
-
-    function turn() {
-        const target = Number(this.id[5]);
-        if (boardState[target] === null) {
-            boardState[target] = activePlayer.marker;
-            this.textContent = activePlayer.marker;
-            setTimeout(endGame, 100); // call before togglePlayer or wrong player will be declared winner
-            togglePlayer();
-        }        
+    else if (win(boardState, 'O')) {
+      playerTwo.increaseScore();
+      playerTwoScore.textContent = playerTwo.getScore();
+      var result = `${playerTwo.name} Wins`;
     }
-
-    function hardReset() {
-        win = false;
-        playerOne.resetScore();
-        playerTwo.resetScore();
-        document.getElementById('player-1-score').textContent = playerOne.getScore();
-        document.getElementById('player-2-score').textContent = playerTwo.getScore();
-        resetGame();
-    }    
-
-    function disableGrid() {
-        gameBoard.board.forEach(cell => cell.removeEventListener('click', turn));
+    else if (draw(boardState)) {
+      var result = "It's a Tie";
     }
+    modal.style.display = 'flex';
+    winnerDisplay.style.display = 'flex';
+    roundWinner.textContent = result;
+  }
 
-    function enableGrid() {
-        gameBoard.board.forEach(cell => cell.addEventListener('click', turn));
+  function turn() {
+    const targetCell = Number(this.id[5]);
+    if (typeof(boardState[targetCell]) === 'number' ) {
+      boardState[targetCell] = activePlayer.marker;
+      this.textContent = activePlayer.marker;
+      console.log(boardState);
+      if (gameOver()) endGameHandler();
+      togglePlayer();
     }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })();
-
-/* ===================================================================================== */
-/* PLAYER */
-
-function playerFactory(name, marker, type='human') {
-    let _score = 0;
-    const increaseScore = () => ++_score;
-    const resetScore = () => _score = 0;
-    const getScore = () => _score;
-    return {
-        name, 
-        marker,
-        type,
-        increaseScore,
-        resetScore,
-        getScore
-    };
-}
